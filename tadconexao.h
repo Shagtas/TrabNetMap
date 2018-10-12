96cf11a365887c10c45d86374f24a2e6890f0a44
Tassio Santuchi <shag44@gmail.com>
2018-10-11 22:01:21 -0300

    Update tadconexao.h

--- tadconexao.h
+++ tadconexao.h
@@ -1,13 +1,26 @@
 #ifndef TADCONEXAO_H
 #define TADCONEXAO_H
 
+#include "tadroteador.h"
+#include "tadterminal.h"
+
 typedef struct tipoconexao TipoConexao;
 
 typedef struct celconexao CelConexao;
 
 typedef struct listConexao ListConexao;
 
-TipoConexao* inicializaTipoConexao(int no);
+ListConexao* criaConexao ();
+
+CelConexao* inicializaCelConexao(ListConexao* no);
+
+void inicializaConRot(TipoRoteador* a, TipoRoteador* b, ListConexao* c);
+
+int seexiste(ListTerminais* aa, char* bb)
+  
+void inicializaConTerl(ListConexao* a, TipoRoteador* b, ListTerminais* c, TipoTerminal* d);
+
+void removeConRot(ListaConexao* a, LisRot* b, char* c);
 
 
 
